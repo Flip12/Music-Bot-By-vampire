5fcec105dac0c11e6dabb218cb5f1d30c1b1fbc8
Youssef Gamal Wahba <106777610+YoussefWahba0@users.noreply.github.com>
2022-06-23 11:14:33 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,5 @@ const client = require("../index");
 client.on('ready', () => {
 console.log(`${client.user.tag}`)
   client.user.setStatus("online")
-    client.user.setActivity(`Type: ${client.config.slash}Play丨Prefix ${client.config.prefix}`, { type: 'LISTENING' })
+    client.user.setActivity(`Type: /Play丨Prefix ${client.config.prefix}`, { type: 'LISTENING' })
 });
